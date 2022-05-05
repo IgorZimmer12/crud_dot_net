ddff728cf01364e42965e50ac8d29f2ee61a8083
Igor Zimmer Silva <igor_zimmer@hotmail.com>
2022-05-05 19:24:33 -0300

    corrigindo bugs

--- Startup.cs
+++ Startup.cs
@@ -23,7 +23,7 @@ namespace ProjetoUsuarios
             services.AddControllersWithViews();
 
             services.AddDbContext<ProjetoUsuariosContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("ProjetoUsuarios")));
+                    options.UseInMemoryDatabase("sqlserver"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
